c2c0dddfef746e2e84ef26e5872fb007e3763223
sayatohenney883 <sayatohenney883@gmail.com>
2026-05-11 13:37:50 -0400

    Update api_handling.py

--- api_handling.py
+++ api_handling.py
@@ -1,1 +1,10 @@
 #api info
+import os
+from dotenv import load_dotenv
+
+# This loads the variables from your .env file into the system environment
+load_dotenv()
+
+# Now you can access them safely
+api_key = os.getenv("FOOD_API_KEY")
+api_id = os.getenv("FOOD_API_ID")
