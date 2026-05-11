2d5adb41dbec9ee2cdc36765beb7f59a3357a977
sayatohenney883 <sayatohenney883@gmail.com>
2026-05-11 13:18:41 -0400

    Update and rename core to core.py

---
+++ core.py
@@ -0,0 +1,10 @@
+while True:
+  try:
+    int(input("Please enter your desired calories for the week here")
+    break
+  except ValueError:
+    print("Not a valid number, try again!")
+try:
+  int(input("Please enter your desired macros for the week here")
+except ValueError
+  float
